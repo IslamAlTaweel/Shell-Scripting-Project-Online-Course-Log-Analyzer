024070de0dd7ebf075b3cfc9139b4e9421ae8086
githublolii <lailanihadawad@gmail.com>
2025-09-11 03:05:20 +0300

    sh1.sh

--- sh1.sh
+++ sh1.sh
@@ -70,7 +70,6 @@ in
             fi
             
        registered=$(cut -d, -f1 "$regFile") #obtain the students registered in the course
-       attended=$(grep "$cID" log.txt | cut -d, -f2) #obtain the students who attended any session in the course
 
        echo "Absent Students in $cID:" #find registered students who never attended a session
        
